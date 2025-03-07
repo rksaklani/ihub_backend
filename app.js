71b438d6d063cc34555e3d34a4b4c9ef0874aba9
Rk Saklani <48331888+rksaklani@users.noreply.github.com>
2025-03-07 23:08:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ require('dotenv').config({ path: __dirname + '/config/config.env' });
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 // Middleware
 app.use(bodyParser.json())
 app.use(express.json()); // Parse JSON request body
